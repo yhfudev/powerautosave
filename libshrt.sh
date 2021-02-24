c4121647a61321ad4bf29d1b274d16f9f1077e20
yhfudev <yhfudev@gmail.com>
2021-02-24 18:25:24 -0500

    rm debug messages.

--- libshrt.sh
+++ libshrt.sh
@@ -72,7 +72,7 @@ mp_remove_child_record() {
   #mr_trace "before remove child ${PARAM_CHILD_ID}, #=${CNTCHILD}, PID list='${PID_CHILDREN}'" 1>&2
   PID_CHILDREN=$(echo ${PID_CHILDREN} | awk -v ID=${PARAM_CHILD_ID} '{for (i = 1; i <= NF; i ++) {if ($i != ID) printf(" %d", $i); } }' )
   CNTCHILD=$(echo ${PID_CHILDREN} | awk '{print NF}' )
-  mr_trace "after remove child ${PARAM_CHILD_ID}, #=${CNTCHILD}, PID list='${PID_CHILDREN}'"
+  #mr_trace "after remove child ${PARAM_CHILD_ID}, #=${CNTCHILD}, PID list='${PID_CHILDREN}'"
 
 }
 
@@ -84,7 +84,7 @@ mp_wait_all_children() {
     for ID2 in ${PID_CHILDREN} ; do
       #mr_trace "wait child ${ID2} ..."
       wait ${ID2} 1>&2
-      mr_trace "child ${ID2} done!"
+      #mr_trace "child ${ID2} done!"
       mp_remove_child_record ${ID2}
     done
     sleep 5
@@ -169,7 +169,7 @@ mp_add_child_check_wait() {
     done
     if [ "$(echo | ${EXEC_AWK} -v A=${CNTCHILD} -v B=${HDFF_NUM_CLONE} '{if(A>=B){print 1;}else{print 0;}}' )" = "1" ]; then
 #if [ 1 = 1 ]; then
-      mr_trace "sleep 1 ..."
+      #mr_trace "sleep 1 ..."
       sleep 1
 #else
 #      #IDX1=$(echo | awk -v S=$RANDOM -v N=$(date +%N) -v M=${CNTCHILD} 'BEGIN{srand(S+N);}{print int(rand()*10*M) % M; }' )

61da37b26f3ecd29a209438a6dfb4390d0e7c1c9
yhfudev <yhfudev@gmail.com>
2021-02-24 00:54:37 -0500

    ping

---
+++ libshrt.sh
@@ -0,0 +1,183 @@
+#!/bin/bash
+# -*- tab-width: 4; encoding: utf-8 -*-
+#
+#####################################################################
+## @file
+## @brief multi-processes bash library
+##
+##   mp_new_session
+##   mp_add_child_check_wait
+##   mp_notify_child_exit
+##   mp_wait_all_children
+##
+## @author Yunhui Fu <yhfudev@gmail.com>
+## @copyright GPL v3.0 or later
+## @version 1
+##
+#####################################################################
+
+# multiple processes support
+CNTCHILD=0
+PID_CHILDREN=
+
+#####################################################################
+EXEC_BASH="$(which bash)"
+if [ ! -x "${EXEC_BASH}" ]; then
+    echo "Error: Not found bash"
+    exit 1
+fi
+
+if [ ! -f "${EXEC_AWK}" ]; then
+  EXEC_AWK=`which gawk`
+fi
+
+if [ ! -f "${EXEC_AWK}" ]; then
+  echo "Error: Not exist awk!" >> "/dev/stderr"
+  exit 1
+fi
+
+EXEC_UUIDGEN="$(which uuidgen)"
+if [ ! -x "${EXEC_UUIDGEN}" ]; then
+    mr_trace "[ERR] not found uuidgen"
+    exit 1
+fi
+#####################################################################
+# use this session id to trace the child process.
+MP_SESSION_ID=
+## @fn mp_new_session()
+## @brief create new process session id
+##
+mp_new_session() {
+  CNTCHILD=0
+  PID_CHILDREN=
+  MP_SESSION_ID=$(uuidgen)
+  mr_trace "generated session id: ${MP_SESSION_ID}"
+}
+
+## @fn mp_get_session_id()
+## @brief get current process session id
+##
+mp_get_session_id() {
+  echo "${MP_SESSION_ID}"
+}
+
+## @fn mp_remove_child_record()
+## @brief remove a child process
+## @param child_id the child process id
+##
+mp_remove_child_record() {
+  PARAM_CHILD_ID=$1
+  shift
+
+  #mr_trace "before remove child ${PARAM_CHILD_ID}, #=${CNTCHILD}, PID list='${PID_CHILDREN}'" 1>&2
+  PID_CHILDREN=$(echo ${PID_CHILDREN} | awk -v ID=${PARAM_CHILD_ID} '{for (i = 1; i <= NF; i ++) {if ($i != ID) printf(" %d", $i); } }' )
+  CNTCHILD=$(echo ${PID_CHILDREN} | awk '{print NF}' )
+  mr_trace "after remove child ${PARAM_CHILD_ID}, #=${CNTCHILD}, PID list='${PID_CHILDREN}'"
+
+}
+
+## @fn mp_wait_all_children()
+## @brief wait all of the children
+mp_wait_all_children() {
+  mr_trace "wait all of children"
+  while [ "$(echo | ${EXEC_AWK} -v A=${CNTCHILD} '{if(A>0){print 1;}else{print 0;}}' )" = "1" ]; do
+    for ID2 in ${PID_CHILDREN} ; do
+      #mr_trace "wait child ${ID2} ..."
+      wait ${ID2} 1>&2
+      mr_trace "child ${ID2} done!"
+      mp_remove_child_record ${ID2}
+    done
+    sleep 5
+  done
+}
+
+#if [ "${DN_DATATMP}" = "" ]; then
+#    DN_DATATMP=.
+#fi
+# ERROR: due to each process will will not share theirs variable,
+#   it would not use the same pid directory, so we don't use file name to indicate the PID of finished child.
+#DN_WAITID=
+#init_multi_processes () {
+  #if [ ! -d "${DN_WAITID}" ]; then
+    # the temp directory to store the quitted PIDs for current environment
+    #DN_WAITID="${DN_DATATMP}/pids-$(uuidgen)"
+    #mkdir -p "${DN_WAITID}"
+    #rm -f "${DN_WAITID}"/*
+  #fi
+#}
+
+## @fn mp_notify_child_exit()
+## @brief the child process notify that its quit.
+## @param child_id the child process id
+##
+## since processes will not share theirs variable values,
+## the children have to use session id from their parent.
+mp_notify_child_exit() {
+  # the session id
+  PARAM_SID=$1
+  shift
+
+  CHILD_ID=${BASHPID}
+  #mr_trace "child notif exit: id=${BASHPID}"
+}
+
+## @fn mp_add_child_check_wait()
+## @brief add new child,
+## @param child_id the child process id
+##
+## check if the children are too many then wait
+mp_add_child_check_wait() {
+  PARAM_CHILD_ID=$1
+  shift
+
+  if [ "${PARAM_CHILD_ID}" = "" ]; then
+    mr_trace "Warning: child id null! ignored"
+    return
+  fi
+  #mr_trace "add child id=${PARAM_CHILD_ID}"
+
+  PID_CHILDREN="${PID_CHILDREN} ${PARAM_CHILD_ID}"
+  CNTCHILD=$(( $CNTCHILD + 1 ))
+
+  if [ "${MP_SESSION_ID}" = "" ]; then
+    fatal_error "not generated session id!"
+    return
+    #mp_generate_session_id
+  fi
+  if [ "${MP_SESSION_ID}" = "" ]; then
+    fatal_error "unable to generate session id!"
+    return
+  fi
+  if [ "${HDFF_NUM_CLONE}" = "" ]; then
+    local NUM_PROC=$(cat /proc/cpuinfo | egrep ^processor | wc -l)
+    HDFF_NUM_CLONE=1
+    if [ "${NUM_PROC}" -gt 1 ] ; then
+        HDFF_NUM_CLONE=$(( ${NUM_PROC} * 8 / 9 ))
+        #HDFF_NUM_CLONE=${NUM_PROC}
+    fi
+  fi
+  #mr_trace "CNTCHILD=${CNTCHILD}; HDFF_NUM_CLONE=${HDFF_NUM_CLONE}, #=${CNTCHILD}, PID list='${PID_CHILDREN}' "
+  while [ "$(echo | ${EXEC_AWK} -v A=${CNTCHILD} -v B=${HDFF_NUM_CLONE} '{if(B<1 || A<B){print 0;}else{print 1;}}' )" = "1" ]; do
+    #echo "[DBG] (self=${BASHPID}) check all of children in the ${DN_DATATMP}/pids-${MP_SESSION_ID}/end/" 1>&2
+    # the number of the end process is no more than HDFF_NUM_CLONE
+    for ID in $PID_CHILDREN ; do
+      ps -p ${ID} > /dev/null 2>&1
+      if [ ! "$?" = "0" ]; then
+        #mr_trace "1 child ${ID} done!"
+        mp_remove_child_record ${ID}
+      fi
+    done
+    if [ "$(echo | ${EXEC_AWK} -v A=${CNTCHILD} -v B=${HDFF_NUM_CLONE} '{if(A>=B){print 1;}else{print 0;}}' )" = "1" ]; then
+#if [ 1 = 1 ]; then
+      mr_trace "sleep 1 ..."
+      sleep 1
+#else
+#      #IDX1=$(echo | awk -v S=$RANDOM -v N=$(date +%N) -v M=${CNTCHILD} 'BEGIN{srand(S+N);}{print int(rand()*10*M) % M; }' )
+#      #ID2=$(echo ${PID_CHILDREN} | awk -v R=${RANDOM} '{IDX=R % NF + 1; print $IDX ;}' )
+#      ID2=$(echo ${PID_CHILDREN} | awk '{print $1;}' )
+#      timeout -k 2s 2s wait ${ID2} &
+#      wait $!
+#fi
+    fi
+  done
+}
--- powerautosave.sh
+++ powerautosave.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # turn system to sleep when system is idle.
 
-
 ################################################################################
 if [ "${FN_LOG}" = "" ]; then
     FN_LOG=mrtrace.log
@@ -18,8 +17,14 @@ fi
 ##
 ## pass a message to log file, and also to stdout
 mr_trace() {
-    #echo "$(date +"%Y-%m-%d %H:%M:%S.%N" | cut -c1-23) [self=${BASHPID},$(basename "$0")] $@" | tee -a ${FN_LOG} 1>&2
-    logger -t powerautosave "$@"
+    echo "$(date +"%Y-%m-%d %H:%M:%S.%N" | cut -c1-23) [self=${BASHPID},$(basename "$0")] $@" | tee -a ${FN_LOG} 1>&2
+    #logger -t powerautosave "$@"
+}
+
+fatal_error() {
+  echo "$(date +"%Y-%m-%d %H:%M:%S.%N" | cut -c1-23) [self=${BASHPID},$(basename "$0")] FATAL: $@" | tee -a ${FN_LOG} 1>&2
+  #logger -t powerautosave "[FATAL] $@"
+  exit 1
 }
 
 ################################################################################
@@ -29,11 +34,251 @@ if [ ! -x "${EXEC_BASH}" ]; then
     exit 1
 fi
 
+EXEC_PRIPS="$(which prips)"
+if [ ! -x "${EXEC_PRIPS}" ]; then
+    mr_trace "[ERR] not found prips"
+    exit 1
+fi
+
+EXEC_IPCALC="$(which ipcalc)"
+if [ ! -x "${EXEC_IPCALC}" ]; then
+    mr_trace "[ERR] not found ipcalc"
+    exit 1
+fi
 
 intall_software() {
   apt update
-  apt -y install bash
+  apt -y install bash prips ipcalc
 }
 
 ################################################################################
+if [ -f "./libshrt.sh" ]; then
+. ./libshrt.sh
+HDFF_NUM_CLONE=16
+fi
+
+# generate session for this process and its children
+#  use mp_get_session_id to get the session id later
+mp_new_session
+
+################################################################################
+
+# detect if the processes is active
+# return 0 on no process
+detect_processes() {
+  local RET=0
+  local PROC=""
+  #mr_trace "PARAM=$@"
+  for PROC in $@; do
+    if [ ! "`pgrep $PROC`" = "" ]; then
+      RET=1
+      break
+    fi
+  done
+  echo "${RET}"
+}
+
+## @fn worker_ping_ip()
+## @brief a worker for ping host
+## @param session_id the session id
+## @param ip the host ip
+## @param fn the file to save the IPs
+## and also the bash library libshrt.sh
+function worker_ping_ip() {
+    local PARAM_SESSION_ID="$1"
+    shift
+    local PARAM_IP="$1"
+    shift
+    local PARAM_FN_LIST="$1"
+    shift
+
+    #mr_trace "ping -c 1 -W 1 ${PARAM_IP} ..."
+    ping -c 1 -W 1 ${PARAM_IP}
+    if [ "$?" = "0" ]; then
+      # awk -v rseed=$RANDOM 'BEGIN{srand(rseed);}{print rand()" "$0}'
+      sleep $( echo | awk -v A=$RANDOM '{printf("%4.3f\n", (A%20+1)*0.3);}' )
+      # push to the list
+      mr_trace "add to list: ${PARAM_IP} ..."
+      echo "${PARAM_IP}" >> ${PARAM_FN_LIST}
+    fi
+
+    mp_notify_child_exit ${PARAM_SESSION_ID}
+}
+mr_trace "here 200000 ..."
+
+## @fn ping_ip_list_from_file()
+## @brief ping the IP from the list in a file
+## @param fn_in the file contains the IPs to be ping
+## @param fn_out the file contains the IPs that are actived
+## return 0 if none actived, 1 if host exists
+ping_ip_list_from_file() {
+  local PARAM_FN_IN=$1
+  shift
+  local PARAM_FN_OUT=$1
+  shift
+  mr_trace "ping_ip_list_from_file ..."
+
+  HDFF_NUM_CLONE=300
+  while read IP; do
+    worker_ping_ip "$(mp_get_session_id)" "${IP}" "${PARAM_FN_OUT}" &
+    PID_CHILD=$!
+    mp_add_child_check_wait ${PID_CHILD}
+  done < "${PARAM_FN_IN}"
+  mp_wait_all_children
+  mr_trace "HDFF_NUM_CLONE=${HDFF_NUM_CLONE}"
+  HDFF_NUM_CLONE=16
+}
+mr_trace "here 300000 ..."
+
+## @fn ping_ip_range()
+## @brief detect the IP range
+## @param ip1 the ip/prefix 1
+## @param ip1 the ip/prefix 2
+## return the detected host IPs
+ping_ip_range() {
+  local PARAM_FN_OUT=$1
+  shift
+  local PARAM_IP1=$1
+  shift
+  local PARAM_IP2=$1
+  shift
+  mr_trace "ping_ip_range ..."
+
+  # ipcalc -b 192.168.0.1/24
+  # prips 192.168.0.1 192.168.1.3
+  if [ "${PARAM_IP2}" = "" ]; then
+    # only ip/prefix
+    IP1=$( ipcalc -b ${PARAM_IP1} | grep "HostMin:" | awk '{print $2}' )
+    IP2=$( ipcalc -b ${PARAM_IP1} | grep "HostMax:" | awk '{print $2}' )
+  else
+    # IP range
+    IP1=$( ipcalc -b ${PARAM_IP1} | grep "Address:" | awk '{print $2}' )
+    IP2=$( ipcalc -b ${PARAM_IP2} | grep "Address:" | awk '{print $2}' )
+  fi
+  local FN_LIST="/tmp/tmp-list-$(uuidgen)"
+  prips $IP1 $IP2 > "${FN_LIST}"
+  ping_ip_list_from_file "${FN_LIST}" "${PARAM_FN_OUT}"
+  #cat "${PARAM_FN_OUT}"
+  rm -f "${FN_LIST}"
+}
+
+LST_ACTIVE_IP=
+## @fn ping_list()
+## @brief ping the IP in the list
+## return 1 if there exist host or 0 if none, and update LST_ACTIVE_IP
+ping_list() {
+  local FN_LIST="/tmp/tmp-list-$(uuidgen)"
+  local FN_OUT="/tmp/tmp-out-$(uuidgen)"
+  echo ${LST_ACTIVE_IP} > "${FN_LIST}"
+  ping_ip_list_from_file "${FN_LIST}" "${FN_OUT}"
+  rm -f "${FN_LIST}"
+  LST_ACTIVE_IP=$( cat "${FN_OUT}" )
+  mr_trace "LST_ACTIVE_IP=${LST_ACTIVE_IP}"
+}
+
+
+## @fn detect_ip()
+## @brief detect the IP range
+## @param ip1 the ip/prefix 1
+## @param ip1 the ip/prefix 2
+## return 1 if there exist host or 0 if none, and set LST_ACTIVE_IP
+detect_ip() {
+  local PARAM_IP1=$1
+  shift
+  local PARAM_IP2=$1
+  shift
+}
+
+detect_hd() {
+  local PARAM_PROC=$1
+  shift
+
+  echo ""
+}
+
+# turn the host to sleep mode
+# mode: one of suspend
+enter_sleep() {
+  PARAM_MODE=$1
+  shift
+
+  mr_trace "enter sleep mode '${PARAM_MODE}' ..."
+  sync && sleep 2 && systemctl ${PARAM_MODE}
+}
+
+# the main loop to detect the background activities
+# if the system is idle, then go to sleep
+do_detect() {
+  mr_trace "do_detect() ..."
+  mr_trace "check if host exists ..."
+  mr_trace "check if CPUs are idle ..."
+  mr_trace "check if disks are idle ..."
+  mr_trace "check if exist background processes ..."
+  mr_trace "wake up at specific time ..."
+  #enter_sleep suspend
+}
+
+################################################################################
+# tests:
+
+assert ()                 #  If condition false,
+{                         #+ exit from script
+                          #+ with appropriate error message.
+  E_PARAM_ERR=98
+  E_ASSERT_FAILED=99
+
+  local PARAM_LINE=$1
+  shift
+  local PARAM_COND=$1
+  shift
+
+  if [ -z "$PARAM_COND" ]; then #  Not enough parameters passed
+                          #+ to assert() function.
+    return $E_PARAM_ERR   #  No damage done.
+  fi
+
+  if [ ! $PARAM_COND ]; then
+    echo "Assertion failed:  \"$PARAM_COND\""
+    echo "File \"$0\", line $PARAM_LINE"    # Give name of file and line number.
+    exit $E_ASSERT_FAILED
+  # else
+  #   return
+  #   and continue executing the script.
+  fi
+} # Insert a similar assert() function into a script you need to debug.
+
+test_detect_processes() {
+  local RET1=0
+
+  RET1=`detect_processes bash`
+  #mr_trace "RET1=${RET1}"
+  assert $LINENO "'${RET1}' = '1'"
+
+  RET1=`detect_processes 'abc'`
+  assert $LINENO "'${RET1}' = '0'"
+}
+
+test_detect_ip() {
+  local FN_LIST="/tmp/tmp-list-$(uuidgen)"
+  rm -f "${FN_LIST}"
+  touch "${FN_LIST}"
+  #ping_ip_range "10.1.1.160/27"
+  ping_ip_range "${FN_LIST}" "10.1.2.1/24"
+  echo "output IP list:"
+  cat "${FN_LIST}"
+  rm -f "${FN_LIST}"
+}
+
+test_all() {
+
+  test_detect_processes
+  test_detect_ip
+
+  mr_trace "Done tests successfully!"
+}
+
+test_all
+
+do_detect
+
 
